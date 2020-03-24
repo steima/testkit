578c8d6d12be5867b5f6bdb1bb965eb3d5bf5e9b
Matthias Steinbauer <matthias@steinbauer.org>
2020-03-24 23:13:24 +0100

    Fixes CF distribution ID for website

--- www/deploy.sh
+++ www/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aws s3 sync --acl public-read --region eu-west-1 ./www/output s3://testkit-www/
-aws cloudfront create-invalidation --distribution-id E16XWH50LONZCS --paths '/*'
+aws cloudfront create-invalidation --distribution-id E12DE9MGRZXN76 --paths '/*'
